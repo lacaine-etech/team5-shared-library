bdc364c0b4ab13514275b7804c28a5a93f62dcad
appbankgroup1 <appbankgroup1@gmail.com>
2023-04-10 22:45:31 -0400

    new groovy file added

--- vars/teamApp.groovy
+++ vars/teamApp.groovy
@@ -1,1 +1,32 @@
-#
+def call(String repoUrl){
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage ('team5-test-shared'){
+            steps{
+                echo "let's test this"
+            }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
